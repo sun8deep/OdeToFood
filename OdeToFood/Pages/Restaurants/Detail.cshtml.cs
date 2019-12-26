c59f4ac2173cc08cf7239543a03b5c840a74c750
sundeep <bvsundeep.cloud@gmail.com>
2019-12-26 09:49:52 +0530

    Commiting main fiels to repository

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+        public void OnGet()
+        {
+
+        }
+    }
+}
